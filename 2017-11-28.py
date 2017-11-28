7ac63be6b2e77f00207512d9ac825718e15c3399
YANGYANTEST <15221629532@163.com>
2017-11-28 15:53:18 +0800

    01练习

--- 2017-11-28.py
+++ 2017-11-28.py
@@ -48,6 +48,15 @@ a=21
 b=10
 c=0
 if(a==b):
-    print("Line 1-a is" equal to b )
+    print("Line 1-a is equal to",b )
 else:
-    print("Line 1-a is" not equal to b)
+    print("Line 1-a is not equal to", b)
+
+if(a!=b):
+    print("Line 2-a is equal to",b )
+else:
+    print("Line 2-a is not equal to", b)
+if( a<>b):
+    print('Line 3-a is equal to',b)
+else:
+    print("Line 3-a is equal to",b)
