f03b7a1302fa054f78712b78ae9e6c7fdb3379da
YANGYANTEST <15221629532@163.com>
2017-11-28 16:08:57 +0800

    01练习

--- 2017-11-28.py
+++ 2017-11-28.py
@@ -60,3 +60,23 @@ if( a<>b):
     print('Line 3-a is equal to',b)
 else:
     print("Line 3-a is equal to",b)
+if(a<b):
+    print("Line 4-a is less than",b)
+else:
+    print("Line 4-a is not less than",b)
+if(a>b):
+    print("Line 5-a is greater than",b)
+else:
+    print("Line 5-a is not greater than",b)
+
+a=5
+b=20
+if(a<=b):
+    print("Line 6 -a is either less than or equal to",b)
+else:
+    print("Line 6 -a is either less than nor equal to",b)
+if(b>=a):
+    print("Line 7 -b is either greater than or equal to ",b)
+else:
+    print("Line 7 -b is either greater than nor equal to ",b)
+  
