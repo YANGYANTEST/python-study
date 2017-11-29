11deaddfa8e84dc422402cd4efcde68363cf7f56
YANGYANTEST <15221629532@163.com>
2017-11-29 13:55:42 +0800

    02练习

---
+++ 2017-11-29.py
@@ -0,0 +1,18 @@
+'''
+字符串的操作
+%s:代表字符串
+%d:数字
+'''
+name="xiaoming"
+address="china"
+age=18
+
+print(name,address)
+
+print(name+" "+address)
+
+# print{"my name is %s,i live in %s,i am %d years old"%(name,address,age)}
+
+#作业：修改以上数据，能够打出浮点数字，如100.126
+
+print(name * 2)
