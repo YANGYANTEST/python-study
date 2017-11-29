39569fc0ea449e1e53c2058d4def4477d801e9f0
YANGYANTEST <15221629532@163.com>
2017-11-29 14:18:16 +0800

    02练习

--- 2017-11-29.py
+++ 2017-11-29.py
@@ -17,10 +17,24 @@ print(name+" "+address)
 #作业：修改以上数据，能够打出浮点数字，如100.126
 print("my name is %s,i live in %s,i am %d years old,i have %d money"%(name,address,age,money))
 
-print("his money is %f"%(100.126))#打印浮点数
-print("i have %.3f money"%(100.12634))#打印浮点数（指定保留小数点位数）
+#打印浮点数
+print("his money is %f"%(100.126))
 
+#打印浮点数（指定保留小数点位数）
+print("i have %.3f money"%(100.12634))
 print ("His height is %f m"%(1.83))
+
+#指定占位符宽度
+print ("Name:%10s Age:%8d Height:%8.2f"%("Aviad",25,1.83))
+
+#指定占位符宽度（左对齐）
+print ("Name:%-10s Age:%-8d Height:%-8.2f"%("Aviad",25,1.83))
+
+#指定占位符（只能用0当占位符）
+print ("Name:%-10s Age:%08d Height:%08.2f"%("Aviad",25,1.83))
+
+#科学计数法
+format(0.0015,'.2e')
 
 
 print(name * 2)
