36cca7bd027caae0279a5f29d71f958f3a79ed56
YANGYANTEST <15221629532@1163.com>
2017-12-08 21:28:06 +0800

    解析CSV文件

---
+++ 2017-12-8.py
@@ -0,0 +1,30 @@
+#解析CSV文件存储数据到列表
+
+#getcwd():查看当前的路径
+
+import os
+import csv
+# print(os.getcwd())
+
+#get csv file path:获取csv的路径
+filepath=os.path.join(os.getcwd(),"date","111.csv")
+#print(filepath)
+
+#open csv file
+f=open(filepath)
+
+users=[]
+#解析数据
+reader=csv.reader(f)
+print(reader)git 
+
+for row in reader:
+    print(reader)
+#把第一行数据删除
+next(reader,None)
+
+#迭代循环
+for row in reader:
+    #print(row)
+    user.append(row)
+print(users)
